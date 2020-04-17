f001bdce205b1685d1bb5a26b9fc4d3a5b7f4594
dontfeedthegoogle <48246628+dontfeedthegoogle@users.noreply.github.com>
2020-04-17 04:29:29 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# franz-recipe-simpleanalytics
-This is the Simple Analytics recipe for Franz.
+# Simple Analytics for Franz 
+Unofficial support for https://simpleanalytics.com/ in Franz.
+
+### How to create your own Franz recipes:
+* [Read the documentation](https://github.com/meetfranz/plugins)
---
+++ webview.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = (Franz) => {
+  function getMessages() {
+    let direct = 0;
+    let indirect = 0;
+    const badgeDiv = document.querySelector('.simpleanalytics').dataset;
+    if (badgeDiv) {
+      const count = parseInt(badgeDiv.innerText);
+      direct = isNaN(count) ? 0 : count;
+    }
+
+    Franz.setBadge(direct, indirect);
+  }
+
+  Franz.loop(getMessages);
+}
